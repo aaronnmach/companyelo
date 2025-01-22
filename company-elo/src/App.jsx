34d1e786708fae3c697a77100b15deaa24018269
amach8 <98847891+amach8@users.noreply.github.com>
2025-01-22 13:25:10 -0800

    created template

---
+++ company-elo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello, React!</h1>
+      </header>
+      <p>
+        Welcome to your new React site. Edit <code>App.jsx</code> to get started.
+      </p>
+    </div>
+  );
+}
+
+export default App;
