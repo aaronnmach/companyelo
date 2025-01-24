3b1e2bdc8df922c1765200f1cbb716e35709ac25
amach8 <98847891+amach8@users.noreply.github.com>
2025-01-24 12:12:22 -0800

    added footer

---
+++ company-elo/src/client/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'; // Add custom styles here if needed
+
+function Footer() {
+    return (
+        <footer className="footer-container">
+            <div className="footer-content">
+                <p>project by anonymous</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
